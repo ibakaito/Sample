25e3b13e5ff343515cb8f14a6006c779d666e681
ibakaito <e1b19009@oit.ac.jp>
2021-10-05 14:54:33 +0900

    y_drive作成

---
+++ src/test/java/y_drive/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package y_drive.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
